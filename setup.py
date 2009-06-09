6db86f2930e1c39049b357aca94eb7db674d5e21
Claes Nästén <claes.nasten@freecode.no>
2009-06-09 20:37:34 +0200

    Start on setup.py for packaging and init scripts.
    Created a setup.py file for easy distribution, while at it setup init scripts.
    This is untested.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# This file is part of plog.
+#
+# plog is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# plog is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with plog.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import plog
+
+from distutils.core import setup
+
+setup(name='plog', version=plog.VERSION,
+      description='python log utlities',
+      author='Claes Nästén', author_email='claes.nasten@freecode.no',
+      url='https://projects.freecode.no/projects/show/plog',
+      packages=['plog', 'plog.file2log', 'plog.file_parsers',
+                'plog.frontend', 'plog.log2db'],
+      scripts=['scripts/file2log', 'scripts/log2db'],
+      data_files=[('sql', ['data/sql/mysql_init.sql']),
+                  ('/etc/init.d', ['data/init/file2log', 'data/init/log2db'])],
+      )
